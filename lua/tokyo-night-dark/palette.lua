6ec2253704990172ea739421c729a1771510e2ed
Ayush Kumar <ayush438500@gmail.com>
2026-05-20 19:21:35 +0530

    fix-3

--- lua/tokyo-night-dark/palette.lua
+++ lua/tokyo-night-dark/palette.lua
@@ -1,27 +1,39 @@
 local palette = {
   none = "NONE",
 
+  ----------------------------------------------------------------
+  -- Base
+  ----------------------------------------------------------------
   bg = "#0a0a0d",
   bg_dark = "#08080b",
+  bg_float = "#101014",
   bg_highlight = "#1e202e",
-  bg_popup = "#101014",
+  bg_popup = "#14141b",
+  bg_sidebar = "#08080b",
   bg_statusline = "#08080b",
-  bg_visual = "#515c7e40",
-  bg_search = "#3d59a166",
-  bg_selection = "#515c7e40",
 
+  bg_visual = "#2a2f44",
+  bg_search = "#364a82",
+  bg_selection = "#2d3248",
+  bg_cursorline = "#1e202e",
+  bg_fold = "#111117",
+
+  ----------------------------------------------------------------
+  -- Foreground
+  ----------------------------------------------------------------
   fg = "#a9b1d6",
   fg_dark = "#787c99",
   fg_gutter = "#363b54",
   fg_comment = "#444b6a",
   fg_alt = "#9aa5ce",
+  fg_inactive = "#515670",
 
   border = "#101014",
 
+  ----------------------------------------------------------------
+  -- Core Colors
+  ----------------------------------------------------------------
   black = "#08080b",
-  gray = "#787c99",
-  gray_dark = "#515670",
-  gray_light = "#acb0d0",
 
   red = "#f7768e",
   red_dark = "#db4b4b",
@@ -36,21 +48,49 @@ local palette = {
   green2 = "#41a6b5",
 
   cyan = "#7dcfff",
-  cyan_dark = "#0db9d7",
   cyan2 = "#89ddff",
+  cyan_dark = "#0db9d7",
   teal = "#2ac3de",
 
   blue = "#7aa2f7",
   blue_dark = "#6183bb",
   blue1 = "#3d59a1",
+  blue2 = "#698cd6",
 
   magenta = "#bb9af7",
   magenta_dark = "#9d7cd8",
   purple = "#b267e6",
 
   white = "#c0caf5",
-  white0 = "#c0cefc",
+  white1 = "#c0cefc",
 
+  gray = "#787c99",
+  gray_dark = "#515670",
+  gray_light = "#acb0d0",
+
+  ----------------------------------------------------------------
+  -- Diagnostics
+  ----------------------------------------------------------------
+  error = "#db4b4b",
+  warning = "#e0af68",
+  info = "#0da0ba",
+  hint = "#0da0ba",
+
+  ----------------------------------------------------------------
+  -- Diff / Git
+  ----------------------------------------------------------------
+  diff_add = "#163b3a",
+  diff_change = "#2a3c5a",
+  diff_delete = "#4a2328",
+  diff_text = "#3d59a1",
+
+  git_add = "#449dab",
+  git_change = "#6183bb",
+  git_delete = "#914c54",
+
+  ----------------------------------------------------------------
+  -- Terminal
+  ----------------------------------------------------------------
   terminal_black = "#363b54",
   terminal_red = "#f7768e",
   terminal_green = "#41a6b5",
@@ -59,19 +99,6 @@ local palette = {
   terminal_magenta = "#bb9af7",
   terminal_cyan = "#7dcfff",
   terminal_white = "#acb0d0",
-
-  diff_add = "#164846",
-  diff_change = "#394b70",
-  diff_delete = "#703438",
-
-  git_add = "#449dab",
-  git_change = "#6183bb",
-  git_delete = "#914c54",
-
-  error = "#db4b4b",
-  warning = "#e0af68",
-  info = "#0da0ba",
-  hint = "#0da0ba",
 }
 
 return palette
