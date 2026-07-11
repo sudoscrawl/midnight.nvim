4b2f88bcb72b37031efae9ee9397dcfe5ed40bd1
Ayush Kumar <ayush438500@gmail.com>
2026-07-11 19:15:39 +0530

    feat: create palette.lua

---
+++ lua/midnight/palette.lua
@@ -0,0 +1,52 @@
+local palette = {
+	-- Backgrounds
+	bg = "#0a0a0d",
+	bg_dark = "#08080b",
+	bg_float = "#101014",
+	bg_alt = "#14141b",
+	bg_highlight = "#1e202e",
+	-- Foregrounds
+	fg = "#a9b1d6",
+	fg_light = "#c0caf5",
+	fg_dark = "#9aa5ce",
+	fg_muted = "#787c99",
+	-- Grays
+	gray = "#515670",
+	gray_dark = "#444b6a",
+	gray_darker = "#363b54",
+	-- Reds
+	red = "#db4b4b",
+	red_light = "#f7768e",
+	red_dark = "#914c54",
+	-- Orange / Yellow
+	orange = "#ff9e64",
+	yellow = "#e0af68",
+	amber = "#c49a5a",
+	-- Greens
+	green = "#73daca",
+	green_light = "#85d0b7",
+	teal = "#41a6b6",
+	teal_dark = "#449dab",
+	green_dark = "#164846",
+	-- Blues
+	blue = "#7aa2f7",
+	blue_dark = "#6183bb",
+	blue_dim = "#394b70",
+	-- Cyans
+	cyan = "#7dcfff",
+	cyan_light = "#89ddff",
+	cyan_dark = "#0db9d7",
+	-- Purples
+	purple = "#bb9af7",
+	purple_dark = "#9d7cd8",
+	-- Pink
+	pink = "#c0768e",
+	-- Misc
+	cursor = "#c0caf5",
+	border = "#101014",
+	comment = "#444b6a",
+	selection = "#515c7e40",
+	none = "NONE",
+}
+
+return palette
